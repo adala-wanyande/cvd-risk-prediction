21f74512406b0511681b6aa22000e2d757cd6fff
adala-wanyande <benardwanyande@gmail.com>
2023-09-20 12:50:04 +0300

    Testing

--- mobile/app/src/main/java/com/example/cvd_risk_prediction/MainActivity.kt
+++ mobile/app/src/main/java/com/example/cvd_risk_prediction/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             CVDRiskPredictionTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Test commit")
+                    Greeting("This the one")
                 }
             }
         }
